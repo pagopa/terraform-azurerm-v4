8c8462c7ee6fe9b2b9193cb043206a6e5679f33c
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-02-18 17:18:32 +0100

    test

--- elastic_app_resources/resources.tf
+++ elastic_app_resources/resources.tf
@@ -43,8 +43,8 @@ resource "elasticstack_elasticsearch_component_template" "custom_index_component
   name     = "${local.application_id}-${each.key}@custom"
   template {
 
-    settings = lookup(each.value.template, "settings", null) != null ? jsonencode(lookup(each.value.template, "settings", null)) : "${jsonencode(each.value)}"
-    mappings = lookup(each.value.template, "mappings", null) != null ? jsonencode(lookup(each.value.template, "mappings", null)) : "{}"
+    settings = lookup(each.value.template, "settings", null) != null ? jsonencode(each.value.template.settings) : "${jsonencode(each.value.template.settings)}"
+    mappings = lookup(each.value.template, "mappings", null) != null ? jsonencode(lookup(each.value.template, "mappings", null)) : "${jsonencode(each.value.template.mappings)}"
   }
 
   metadata = jsonencode(lookup(each.value, "_meta", null))
