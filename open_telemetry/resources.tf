710bda7508e1b3a2b69e1720816df721d0d38e28
ffppa <fabio.felici@pagopa.it>
2025-03-03 14:42:29 +0100

    chore: Update paths and defaults; rename synthetic dashboard file (#19)
    Update paths and defaults; rename synthetic dashboard file
    
    Updated module paths for template files and introduced a default Grafana URL to improve clarity and usability. Renamed the synthetic monitoring template file for consistency. Adjusted formatting in OpenTelemetry resources for better readability.

--- open_telemetry/resources.tf
+++ open_telemetry/resources.tf
@@ -29,11 +29,11 @@ resource "kubectl_manifest" "otel_collector" {
     helm_release.opentelemetry_operator_helm
   ]
   yaml_body = templatefile("${path.module}/yaml/collector.yaml", {
-    namespace         = var.otel_kube_namespace
-    apm_api_key       = var.elasticsearch_api_key
-    apm_endpoint      = var.elasticsearch_apm_host
-    receiver_port     = var.grpc_receiver_port
-    deployment_env    = var.deployment_env
+    namespace      = var.otel_kube_namespace
+    apm_api_key    = var.elasticsearch_api_key
+    apm_endpoint   = var.elasticsearch_apm_host
+    receiver_port  = var.grpc_receiver_port
+    deployment_env = var.deployment_env
   })
 
   force_conflicts = true
