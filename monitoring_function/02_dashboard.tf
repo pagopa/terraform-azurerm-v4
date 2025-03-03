f76c56b00d655ef4bca0f7e5a5155d635bfe5f73
Fabio Felici <fabio.felici@pagopa.it>
2025-03-03 14:30:54 +0100

    Update paths and defaults; rename synthetic dashboard file
    Updated module paths for template files and introduced a default Grafana URL to improve clarity and usability. Renamed the synthetic monitoring template file for consistency. Adjusted formatting in OpenTelemetry resources for better readability.

--- monitoring_function/02_dashboard.tf
+++ monitoring_function/02_dashboard.tf
@@ -10,7 +10,7 @@ resource "grafana_dashboard" "sythetic_monitoring" {
   provider = grafana.cloud
 
   folder = grafana_folder.sythetic_monitoring[0].uid
-  config_json = templatefile("./dashboards/synthetic_monitoring.tpl", {
+  config_json = templatefile("${path.module}/dashboards/synthetic_monitoring.tpl", {
     subscription_id           = var.subscription_id
     resource_group_name       = var.application_insight_rg_name
     application_insights_name = var.application_insight_name
