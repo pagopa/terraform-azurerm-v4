977b1b4f1a325335aea7d0975940b720c3f381f9
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2025-03-19 12:21:37 +0100

    fix

--- grafana_dashboard/outputs.tf
+++ grafana_dashboard/outputs.tf
@@ -1,5 +1,5 @@
 output "risorse" {
-  value = data.azurerm_resources.sub_resources.required_tags
+  value = toset(data.azurerm_resources.sub_resources.required_tags)
 }
 output "localrisorse" {
   value = local.allowed_resource_type
