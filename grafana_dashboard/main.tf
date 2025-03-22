5cc1b9e19b009f53b54506a76aefc1d548912e74
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2025-03-22 16:37:21 +0100

    subfolder

--- grafana_dashboard/main.tf
+++ grafana_dashboard/main.tf
@@ -41,8 +41,8 @@ locals {
   dashboard_subfolder_map = flatten([
     for rt in data.azurerm_resources.sub_resources : [
       for d in rt.resources : {
-        domain_exists = lookup(d.tags, "domain", "nodomain")
-        type = split("/", d.type)[1]
+        subdomain_exists = lookup(d.tags, "domain", "nodomain")-split("/", d.type)[1]
+        //type = split("/", d.type)[1]
       }
     ]
   ])
@@ -70,10 +70,10 @@ resource "grafana_folder" "domainsfolderexist" {
 
 resource "grafana_folder" "domainsfolder" {
   provider = grafana.cloud
-  for_each = { for i in range(length(distinct(local.dashboard_subfolder_map))) : format("%s-%s",local.dashboard_subfolder_map[i].domain_exists,local.dashboard_subfolder_map[i].type) => i }
+  for_each = { for i in range(length(distinct(local.dashboard_subfolder_map))) : local.dashboard_subfolder_map[i].subdomain_exists => i }
 
-  parent_folder_uid = grafana_folder.domainsfolderexist["${local.dashboard_resource_map[each.value].domain_exists}"].uid
-  title = "${upper(local.dashboard_subfolder_map[each.value].domain_exists)}-${local.dashboard_subfolder_map[each.value].type}"
+  parent_folder_uid = grafana_folder.domainsfolderexist["${split("-",local.dashboard_subfolder_map[each.value].subdomain_exists[0])}"].uid
+  title = "${upper(split("-",local.dashboard_subfolder_map[each.value].subdomain_exists[0]))}-${split("-",local.dashboard_subfolder_map[each.value].subdomain_exists[1])}"
 }
 
 
