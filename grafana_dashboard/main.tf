c97a5fb7c56f9ea79100b699a567eb36d9837d3e
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2025-03-22 16:19:18 +0100

    fix

--- grafana_dashboard/main.tf
+++ grafana_dashboard/main.tf
@@ -61,7 +61,7 @@ resource "grafana_folder" "domainsfolderexist" {
 
 resource "grafana_folder" "domainsfolder" {
   provider          = grafana.cloud
-  for_each          = { for i in range(length(distinct(local.dashboard_resource_map))) : format("%s-%s", local.dashboard_resource_map[i].domain_exists, local.dashboard_resource_map[i].type) => i }
+  for_each          = { for i in range(length(local.dashboard_resource_map)) : distinct(format("%s-%s", local.dashboard_resource_map[i].domain_exists, local.dashboard_resource_map[i].type)) => i }
   parent_folder_uid = grafana_folder.domainsfolderexist["${local.dashboard_resource_map[each.value].domain_exists}"].uid
 
   title = "${upper(local.dashboard_resource_map[each.value].domain_exists)}-${split("/", local.dashboard_resource_map[each.value].type)[1]}"
