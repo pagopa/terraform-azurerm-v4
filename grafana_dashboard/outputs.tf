76093f06aa2fd1fc00b30565def028a2230b7802
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2025-03-22 16:52:59 +0100

    debug

--- grafana_dashboard/outputs.tf
+++ grafana_dashboard/outputs.tf
@@ -1,7 +1,7 @@
 output "subfolder" {
-  value= local.dashboard_subfolder_map
+  value= distinct(local.dashboard_subfolder_map)
 }
 
 output "folder" {
-  value= local.dashboard_folder_map
+  value= distinct(local.dashboard_folder_map)
 }
