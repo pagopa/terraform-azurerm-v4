96dc63fc8914b01097e86f9cc47f040e80c57893
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2025-04-02 14:41:35 +0200

    upgrade cert mounter

--- cert_mounter/main.tf
+++ cert_mounter/main.tf
@@ -2,13 +2,12 @@ resource "helm_release" "cert_mounter" {
   name         = replace(substr(var.helm_release_name, 0, 53), ".", "-")
   repository   = "https://pagopa.github.io/aks-helm-cert-mounter-blueprint"
   chart        = "cert-mounter-blueprint"
-  version      = local.chart_version
+  version      = var.cert_mounter_chart_version
   namespace    = var.namespace
   timeout      = 120
   force_update = true
 
   values = [
-    var.workload_identity_enabled ?
     templatefile("${path.module}/helm/cert-mounter-workload-identity.yaml.tpl", {
       NAMESPACE                   = var.namespace,
       CERTIFICATE_NAME            = var.certificate_name,
@@ -18,12 +17,8 @@ resource "helm_release" "cert_mounter" {
       WORKLOAD_IDENTITY_CLIENT_ID = var.workload_identity_client_id
       POD_RAM                     = var.pod_ram
       POD_CPU                     = var.pod_cpu
-    }) :
-    templatefile("${path.module}/helm/cert-mounter-pod-identity.yaml.tpl", {
-      NAMESPACE        = var.namespace,
-      CERTIFICATE_NAME = var.certificate_name,
-      KEY_VAULT_NAME   = var.kv_name
-      TENANT_ID        = var.tenant_id
+      TOLERATIONS                 = var.tolerations
+      AFFINITY                    = var.affinity
     })
   ]
 }
