83b098767e30a5f1172f343ea671b7f88cc25cd2
Fabio Felici <fabio.felici@pagopa.it>
2025-04-09 12:11:31 +0200

    Update lifecycle ignore_changes for load balancer profile
    Refined the ignored attribute in the lifecycle block to target the specific `idle_timeout_in_minutes` field within the load balancer profile. This ensures more precise and controlled handling of resource changes in Terraform.

--- kubernetes_cluster/01_main.tf
+++ kubernetes_cluster/01_main.tf
@@ -181,7 +181,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   lifecycle {
     ignore_changes = [
       default_node_pool[0].node_count,
-      network_profile[0].load_balancer_profile,
+      network_profile[0].load_balancer_profile[0].idle_timeout_in_minutes,
     ]
   }
 
