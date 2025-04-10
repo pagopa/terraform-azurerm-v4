2b4e75b13ff24dff3eee0a64dcc2062ee1b363ed
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-10 14:43:41 +0200

    feat: Add outputs for virtual network and resource group names in Terraform

--- subnet/outputs.tf
+++ subnet/outputs.tf
@@ -17,3 +17,11 @@ output "subnet_name" {
 output "address_prefixes" {
   value = azurerm_subnet.this.address_prefixes
 }
+
+output "virtual_network_name" {
+  value = azurerm_subnet.this.virtual_network_name
+}
+
+output "resource_group_name" {
+  value = azurerm_subnet.this.resource_group_name
+}
