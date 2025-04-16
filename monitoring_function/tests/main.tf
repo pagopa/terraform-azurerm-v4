c7dd7526521c1dc79a62a53ef68c3376814ae1e4
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-16 16:06:06 +0200

    minor fix

--- monitoring_function/tests/main.tf
+++ monitoring_function/tests/main.tf
@@ -17,7 +17,6 @@ locals {
   product = "${var.prefix}-${var.env_short}"
   domain  = "synthetic"
 
-
   monitor_appinsights_name        = "${local.product}-appinsights"
   monitor_action_group_slack_name = "SlackPagoPA"
   monitor_action_group_email_name = "PagoPA"
@@ -27,6 +26,4 @@ locals {
   vnet_core_name                              = "${local.product}-vnet"
   log_analytics_workspace_name                = "${local.product}-law"
   log_analytics_workspace_resource_group_name = "${local.product}-monitor-rg"
-
-
 }
