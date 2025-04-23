873d78eb522548d31b74cb1cae1e2802e29edb76
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-04-23 16:28:31 +0200

    update module

--- network_security_group/main.tf
+++ network_security_group/main.tf
@@ -1,98 +1,3 @@
-locals {
-  default_nsg = {
-    my-postgres-nsg = {
-      type = "postgres" //libreria di type predefiniti per servizi più usati, 443, cosmos, eventhub, redis
-
-      source_subnet_name = "my-subnet"
-      source_subnet_vnet_name = "my-vnet"
-
-      destination_subnet_name = "my-subnet"
-      destination_subnet_vnet_name = "my-vnet"
-
-    }
-  }
-
-  vnets = [{
-      name = "my-vnet"
-      rg_name = "my-vnet-rg"
-    }]
-      //lista di data di tutte le vnet della subscription
-
-
-  nsg = {
-    my-aks-nsg = {
-      inbound_rules = [
-        {
-          name                       = "my-inbound-1"
-          priority                   = 100
-          access                     = "Allow"
-          protocol                   = "*"
-
-          source_subnet_name = "my-subnet"
-          source_subnet_vnet_name = "my-vnet"
-
-          destination_subnet_name = "my-subnet"
-          destination_subnet_vnet_name = "my-vnet"
-
-          source_port_ranges = []
-          destination_port_ranges = ["80"]
-
-          source_application_security_group_ids = []
-          destination_application_security_group_ids = []
-        }
-      ]
-
-      outbound_rules = [
-        {
-          name                       = "my-outbound-1"
-          priority                   = 100
-          access                     = "Allow"
-          protocol                   = "*"
-
-          source_subnet_name = "my-subnet"
-          source_subnet_vnet_name = "my-vnet"
-
-          destination_subnet_name = "my-subnet"
-          destination_subnet_vnet_name = "my-vnet"
-
-          source_port_ranges = []
-          destination_port_ranges = ["80"]
-
-          source_application_security_group_ids = []
-          destination_application_security_group_ids = []
-
-        }
-      ]
-
-    }
-  }
-}
-
-
-data "azurerm_virtual_network" "vnet" {
-  for_each = { for vnet in local.vnets : vnet.name => vnet }
-
-  name                = each.value.name
-  resource_group_name = each.value.rg_name
-}
-
-
-locals {
-  subnets = flatten([
-    for vnet in data.azurerm_virtual_network.vnet :
-    [
-      for subnet in vnet.subnet :
-      {
-        name = subnet.name
-        vnet_name = vnet.name
-        id   = subnet.id
-        address_prefixes  = subnet.address_prefixes
-      }
-    ]
-  ])
-}
-
-
 resource "azurerm_network_security_group" "custom_nsg" {
   for_each = var.custom_security_group
 
@@ -100,62 +5,63 @@ resource "azurerm_network_security_group" "custom_nsg" {
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  dynamic "security_rule" {
-    for_each = each.value.inbound_rules
-    content {
-      name                       = security_rule.value.name
-      priority                   = security_rule.value.priority
-      direction                  = "Inbound"
-      access                     = security_rule.value.access
-      protocol                   = security_rule.value.protocol
-      source_port_ranges         = security_rule.value.source_port_ranges
-      destination_port_ranges    = security_rule.value.destination_port_ranges
-      source_address_prefix      = local.subnets[index(local.subnets.*.name, security_rule.value.source_subnet_name)].address_prefixes
-      destination_address_prefix = local.subnets[index(local.subnets.*.name, security_rule.value.destination_subnet_name)].address_prefixes
-    }
-  }
+  tags = var.tags
+}
 
-  dynamic "security_rule" {
-    for_each = each.value.outbound_rules
-    content {
-      name                       = security_rule.value.name
-      priority                   = security_rule.value.priority
-      direction                  = "Outbound"
-      access                     = security_rule.value.access
-      protocol                   = security_rule.value.protocol
-      source_port_ranges         = security_rule.value.source_port_ranges
-      destination_port_ranges    = security_rule.value.destination_port_ranges
-      source_address_prefix      = local.subnets[index(local.subnets.*.name, security_rule.value.source_subnet_name)].address_prefixes
-      destination_address_prefix = local.subnets[index(local.subnets.*.name, security_rule.value.destination_subnet_name)].address_prefixes
-    }
-  }
+resource "azurerm_network_security_rule" "custom_security_rule" {
+  for_each = { for sr in local.security_rules : "${sr.nsg_name}-${sr.name}" => sr }
+
+  name                         = each.value.name
+  priority                     = each.value.priority
+  direction                    = each.value.direction
+  access                       = each.value.access
+  protocol                     = each.value.protocol
+  source_port_range            = each.value.source_port_range
+  source_port_ranges           = each.value.source_port_ranges
+  destination_port_range       = each.value.destination_port_range
+  destination_port_ranges      = each.value.destination_port_ranges
+  source_address_prefix        = each.value.source_address_prefix
+  source_address_prefixes      = each.value.source_address_prefixes
+  destination_address_prefix   = each.value.destination_address_prefix
+  destination_address_prefixes = each.value.destination_address_prefixes
+
+  resource_group_name = var.resource_group_name
+
+  network_security_group_name = azurerm_network_security_group.custom_nsg[each.value.nsg_name].name
+}
 
 
-  tags                = var.tags
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  for_each = var.custom_security_group
 
+  subnet_id                 = data.azurerm_subnet.subnet["${each.value.target_subnet_name}-${each.value.target_subnet_vnet_name}"].id
+  network_security_group_id = azurerm_network_security_group.custom_nsg[each.key].id
 }
 
 
 
-# resource "azurerm_network_watcher_flow_log" "test" {
-#   network_watcher_name = azurerm_network_watcher.test.name
-#   resource_group_name  = azurerm_resource_group.example.name
-#   name                 = "example-log"
-#
-#   target_resource_id = azurerm_network_security_group.test.id
-#   storage_account_id = azurerm_storage_account.test.id
-#   enabled            = true
-#
-#   retention_policy {
-#     enabled = true
-#     days    = 7
-#   }
-#
-#   traffic_analytics {
-#     enabled               = true
-#     workspace_id          = azurerm_log_analytics_workspace.test.workspace_id
-#     workspace_region      = azurerm_log_analytics_workspace.test.location
-#     workspace_resource_id = azurerm_log_analytics_workspace.test.id
-#     interval_in_minutes   = 10
-#   }
-# }
+resource "azurerm_network_watcher_flow_log" "network_watcher_flow_log" {
+  for_each = var.custom_security_group
+
+  network_watcher_name = var.flow_logs.network_watcher_name
+  resource_group_name  = var.flow_logs.network_watcher_rg
+  name                 = "${var.prefix}-${each.key}-flow-log"
+
+  target_resource_id = azurerm_network_security_group.custom_nsg[each.key].id
+  storage_account_id        = var.flow_logs.storage_account_id
+  enabled                   = each.value.watcher_enabled
+
+  retention_policy {
+    enabled = true
+    days    = var.flow_logs.retention_days
+  }
+
+  traffic_analytics {
+    enabled               = each.value.watcher_enabled
+    workspace_id          = data.azurerm_log_analytics_workspace.analytics_workspace.workspace_id
+    workspace_region      = data.azurerm_log_analytics_workspace.analytics_workspace.location
+    workspace_resource_id = data.azurerm_log_analytics_workspace.analytics_workspace.id
+    interval_in_minutes   = var.flow_logs.traffic_analytics_law_interval_minutes
+  }
+
+}
