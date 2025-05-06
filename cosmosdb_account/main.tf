2aaaec204b1113a9d56d6dbef2aa259e130151ce
Fabio Felici <fabio.felici@pagopa.it>
2025-05-06 14:45:54 +0200

    Add minimal_tls_version variable to CosmosDB module
    Introduced a new variable `minimal_tls_version` to specify the minimum TLS version for a CosmosDB account, defaulting to TLS 1.2. This enhances security configuration by providing flexibility in enforcing appropriate TLS versions.

--- cosmosdb_account/main.tf
+++ cosmosdb_account/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_cosmosdb_account" "this" {
   free_tier_enabled          = var.enable_free_tier
   automatic_failover_enabled = var.enable_automatic_failover
   key_vault_key_id           = var.key_vault_key_id
+  minimal_tls_version        = var.minimal_tls_version
 
   mongo_server_version   = var.mongo_server_version
   burst_capacity_enabled = var.burst_capacity_enabled
