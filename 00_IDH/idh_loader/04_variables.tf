f030affc8434268377b1cf4a7594d638d33b5d25
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-13 09:59:21 +0200

    wip yml

--- 00_IDH/idh_loader/04_variables.tf
+++ 00_IDH/idh_loader/04_variables.tf
@@ -29,7 +29,7 @@ variable "idh_category" {
   description = "(Required) The IDH resource category to be created."
 
   validation {
-    condition     = can(keys(local.local_data))
+    condition     = can(file("${path.module}/../idh/${var.prefix}/${var.env}/${var.idh_category}.yml"))
     error_message = "Specified idh_category not available in catalog for given prefix and env"
   }
 
