b69c6f313ff4a05fa06d5dcee5de6e6c30f8968b
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-13 09:48:18 +0200

    wip yml

--- 00_IDH/idh_loader/01_main.tf
+++ 00_IDH/idh_loader/01_main.tf
@@ -1,6 +1,6 @@
 locals {
   # local_data = jsondecode(file("${path.module}/../idh/${var.prefix}/${var.env}/idh.json"))
-  local_data = yamldecode(file("${path.module}/../idh/${var.prefix}/${var.env}/idh.json"))
+  local_data = yamldecode(file("${path.module}/../idh/${var.prefix}/${var.env}/idh.yml"))
 }
 
 
