7ade78412e0725bf203f11e445d696b82880ee76
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-14 15:49:03 +0200

    wip pg params

--- IDH/postgres_flexible_server/03_outputs.tf
+++ IDH/postgres_flexible_server/03_outputs.tf
@@ -24,6 +24,6 @@ output "administrator_password" {
 }
 
 output "connection_port" {
-  value     = module.idh_loader.idh_config.pgbouncer_enabled ? "6432" : "5432"
+  value     = module.idh_loader.idh_config.server_parameters.pgbouncer_enabled ? "6432" : "5432"
   sensitive = false
 }
