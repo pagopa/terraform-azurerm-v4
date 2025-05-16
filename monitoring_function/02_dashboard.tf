2e476c2575a948af2ed85cfca78b22e98f189127
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-05-16 15:49:09 +0200

    fix path for synthetic_monitoring.json.tpl in dashboard configuration

--- monitoring_function/02_dashboard.tf
+++ monitoring_function/02_dashboard.tf
@@ -8,7 +8,7 @@ resource "grafana_dashboard" "sythetic_monitoring" {
   count = var.enabled_sythetic_dashboard ? 1 : 0
 
   folder = grafana_folder.sythetic_monitoring[0].uid
-  config_json = templatefile("synthetic_monitoring.json.tpl", {
+  config_json = templatefile("${path.module}/dashboards/synthetic_monitoring.json.tpl", {
     subscription_id           = var.subscription_id
     resource_group_name       = var.application_insight_rg_name
     application_insights_name = var.application_insight_name
