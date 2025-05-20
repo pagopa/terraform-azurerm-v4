22f3be450ea1a0ff6241950237594eeac47dd3d2
Fabio Felici <fabio.felici@pagopa.it>
2025-05-20 17:09:30 +0200

    Refactor script to use updated documentation generator.
    Updated the script to call `.scripts/idh_doc_gen.py` instead of `IDH/01_utils/doc_gen.py`. This ensures compatibility with the new location and structure of the documentation generator script.

--- .scripts/generate-idh-docs.sh
+++ .scripts/generate-idh-docs.sh
@@ -3,6 +3,6 @@
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -W "ignore" -m pip install pyyaml --break-system-packages
-python3 -W "ignore" IDH/01_utils/doc_gen.py
+python3 -W "ignore" .scripts/idh_doc_gen.py
 deactivate
 rm -rf .venv
