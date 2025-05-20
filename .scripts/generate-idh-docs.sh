d82292d3fe67376800879eba41403312eb44cb4f
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-20 17:50:14 +0200

    wip

--- .scripts/generate-idh-docs.sh
+++ .scripts/generate-idh-docs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-tree -R -d -a .
+ls -lart
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install pyyaml
 python3 .scripts/idh_doc_gen.py
 deactivate
 rm -rf .venv
-tree -R -d -a .
+ls -lart
