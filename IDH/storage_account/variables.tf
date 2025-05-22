0ac4160f54beed14986ed6e88a4140b7987fcc09
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-05-22 18:05:48 +0200

    wip

--- IDH/storage_account/variables.tf
+++ IDH/storage_account/variables.tf
@@ -141,7 +141,7 @@ variable "replication_type" {
 
   validation {
     condition = try(local.allowed_replication_types[var.replication_type], -1) >= local.allowed_replication_types[module.idh_loader.idh_config.min_account_replication_type]
-    error_message = "The replication type '${var.replication_type}' is not allowed in '${var.env}' environment for '${var.idh_resource}'. The minimum replication type is '${module.idh_loader.idh_config.min_account_replication_type}'. Valid values are ${join(\",\",keys(local.allowed_replication_types))}"
+    error_message = "The replication type '${var.replication_type}' is not allowed in '${var.env}' environment for '${var.idh_resource}'. The minimum replication type is '${module.idh_loader.idh_config.min_account_replication_type}'. Valid values are ${join(\\",\\",keys(local.allowed_replication_types))}"
   }
 }
 
