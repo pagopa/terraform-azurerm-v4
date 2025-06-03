5e7218c3a7b7b080dfbba45b4f4b97f63cd9d05b
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-06-03 12:27:13 +0200

    minro fix

--- IDH/key_vault_access_policy/outputs.tf
+++ IDH/key_vault_access_policy/outputs.tf
@@ -1,4 +1,4 @@
 output "access_policy_id" {
-  description = "L'ID della policy creata"
+  description = "Access Policy ID for the Key Vault"
   value       = azurerm_key_vault_access_policy.this.id
 }
