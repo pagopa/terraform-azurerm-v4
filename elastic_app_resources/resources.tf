11f8fb0ab31cef690eaba900562564962d95dca3
Marco Mari <130982006+mamari90@users.noreply.github.com>
2025-06-10 21:50:04 +0200

    feat: Added custom index component parameters (#88)
    * custom index component parameters

--- elastic_app_resources/resources.tf
+++ elastic_app_resources/resources.tf
@@ -6,12 +6,11 @@ locals {
 
   elastic_namespace = "${var.target_name}.${var.target_env}"
 
-  index_custom_component = { for k, v in var.configuration.indexTemplate : k => jsondecode(templatefile("${var.library_index_custom_path}/${lookup(v, "customComponent", var.default_custom_component_name)}.json", {
-    name                = "${k}-${local.application_id}"
-    pipeline            = elasticstack_elasticsearch_ingest_pipeline.ingest_pipeline[k].name
-    lifecycle           = "${var.target_name}-${var.target_env}-${var.ilm_name}-ilm"
-    primary_shard_count = var.primary_shard_count
-  })) }
+  index_custom_component = { for k, v in var.configuration.indexTemplate : k => jsondecode(templatefile("${var.library_index_custom_path}/${lookup(v, "customComponent", var.default_custom_component_name)}.json", merge({
+    name      = "${k}-${local.application_id}"
+    pipeline  = elasticstack_elasticsearch_ingest_pipeline.ingest_pipeline[k].name
+    lifecycle = "${var.target_name}-${var.target_env}-${var.ilm_name}-ilm"
+  }, var.custom_index_component_parameters))) }
 
   index_package_component = { for k, v in var.configuration.indexTemplate : k => jsondecode(templatefile("${var.library_index_package_path}/${v.packageComponent}.json", {
     name = "${k}-${local.application_id}"
