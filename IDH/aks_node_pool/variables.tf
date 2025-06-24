e66749b58f1f2a9e1d14d91253b4a60d0ce082ba
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-06-24 15:14:38 +0200

    fix variable optional and required

--- IDH/aks_node_pool/variables.tf
+++ IDH/aks_node_pool/variables.tf
@@ -1,31 +1,31 @@
 variable "product_name" {
   type        = string
-  description = "(Prefix) Required: Product name used to identify the platform for which the resource will be created."
+  description = "(Required): Product name used to identify the platform for which the resource will be created."
 }
 
 variable "env" {
   type        = string
-  description = "(Prefix) Required: Environment for which the resource will be created."
+  description = "(Required): Environment for which the resource will be created."
 }
 
 variable "idh_resource_tier" {
   type        = string
-  description = "(Prefix) Required: The name of IDH resource tier to be created."
+  description = "(Required): The name of IDH resource tier to be created."
 }
 
 variable "kubernetes_cluster_id" {
   type        = string
-  description = "(Prefix) Required: AKS cluster id."
+  description = "(Required): AKS cluster id."
 }
 
 variable "vnet_subnet_id" {
   type        = string
-  description = "(Prefix) Required: Subnet id for the node pool."
+  description = "(Required): Subnet id for the node pool."
 }
 
 variable "name" {
   type        = string
-  description = "(Prefix) Required: Node pool name. Must not exceed 12 characters."
+  description = "(Required): Node pool name. Must not exceed 12 characters."
   validation {
     condition     = length(var.name) <= 12
     error_message = "The node pool name must not exceed 12 characters."
@@ -34,82 +34,80 @@ variable "name" {
 
 variable "tags" {
   type        = map(any)
-  description = "(Prefix) Optional: Map of tags to assign to the resource."
+  description = "(Optional): Map of tags to assign to the resource."
 }
 
 variable "vm_size" {
   type        = string
   default     = null
-  description = "(Prefix) Optional: The size of the Virtual Machine."
+  description = "(Optional): The size of the Virtual Machine."
 }
 
 variable "os_disk_type" {
   type        = string
   default     = null
-  description = "(Prefix) Optional: The type of the OS disk."
+  description = "(Optional): The type of the OS disk."
 }
 
 variable "os_disk_size_gb" {
   type        = number
-  description = "(Prefix) Optional: The size of the OS disk in GB."
+  default = null
+  description = "(Optional): The size of the OS disk in GB."
 }
 
 variable "zones" {
   type        = list(string)
   default     = null
-  description = "(Prefix) Optional: List of availability zones where the node pool should be deployed."
+  description = "(Optional): List of availability zones where the node pool should be deployed."
 }
 
 variable "ultra_ssd_enabled" {
   type        = bool
   default     = null
-  description = "(Prefix) Optional: Enable ultra SSD for the node pool."
+  description = "(Optional): Enable ultra SSD for the node pool."
 }
 
 variable "enable_host_encryption" {
   type        = bool
   default     = null
-  description = "(Prefix) Optional: Enable host encryption for the node pool."
+  description = "(Optional): Enable host encryption for the node pool."
 }
 
 variable "node_count_min" {
   type        = number
   default     = null
-  description = "(Prefix) Optional: Minimum number of nodes in the node pool."
+  description = "(Optional): Minimum number of nodes in the node pool."
 }
 
 variable "node_count_max" {
   type        = number
   default     = null
-  description = "(Prefix) Optional: Maximum number of nodes in the node pool."
+  description = "(Optional): Maximum number of nodes in the node pool."
 }
 
 variable "max_pods" {
   type        = number
   default     = null
-  description = "(Prefix) Optional: Maximum number of pods per node."
+  description = "(Optional): Maximum number of pods per node."
 }
 
 variable "node_labels" {
   type        = map(string)
-  default     = {}
-  description = "(Prefix) Optional: Map of labels to assign to the nodes."
+  description = "(Required): Map of labels to assign to the nodes."
 }
 
 variable "node_taints" {
   type        = list(string)
-  default     = null
-  description = "(Prefix) Optional: List of taints to assign to the nodes."
+  description = "(Required): List of taints to assign to the nodes."
 }
 
 variable "upgrade_settings_max_surge" {
   type        = string
   default     = null
-  description = "(Prefix) Optional: Max surge for node pool upgrades."
+  description = "(Optional): Max surge for node pool upgrades."
 }
 
 variable "node_tags" {
   type        = map(any)
-  default     = {}
-  description = "(Prefix) Optional: Map of tags to assign to the nodes."
+  description = "(Required): Map of tags to assign to the nodes."
 }
