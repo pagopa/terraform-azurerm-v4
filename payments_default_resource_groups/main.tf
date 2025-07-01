ebd0d1a5e405be41b878d452fab0ece3d1bbdeb1
diegoitaliait <diego.lagosmorales@pagopa.it>
2025-07-01 11:41:26 +0200

    feat: Added identity rg into default resource groups (#110)
    * added identity default folder
    
    * pre-commit fixs

--- payments_default_resource_groups/main.tf
+++ payments_default_resource_groups/main.tf
@@ -6,6 +6,7 @@ locals {
     compute    = "${var.resource_group_prefix}-compute-rg"
     cicd       = "${var.resource_group_prefix}-cicd-rg"
     monitoring = "${var.resource_group_prefix}-monitoring-rg"
+    identity   = "${var.resource_group_prefix}-identity-rg"
   }
 
   # Merge default and additional resource groups
