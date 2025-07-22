984348113b5dfcb5e303d07147550523c0ae21fb
Fabio Felici <fabio.felici@pagopa.it>
2025-07-22 14:53:42 +0200

    Add capacity variable and update Redis configuration in cstar production

--- IDH/redis/variables.tf
+++ IDH/redis/variables.tf
@@ -93,3 +93,13 @@ variable "patch_schedules" {
   default     = null
   description = "(Optional) List of day-time where Azure can start the maintenance activity"
 }
+
+variable "capacity" {
+  type        = number
+  default     = null
+  description = "(Required) The size of the Redis cache to deploy. Valid values are 0, 1, 2, 3, 4, 5 and 6 for Basic/Standard SKU and 1, 2, 3, 4 for Premium SKU."
+  validation {
+    condition     = contains([0, 1, 2, 3, 4, 5, 6], var.capacity)
+    error_message = "The capacity value must be one of: 0, 1, 2, 3, 4, 5, 6"
+  }
+}
