3b8a515b0fdd81a0cd219fb52de2ea769f97ac7f
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2025-07-29 16:10:10 +0000

    fix: gh runner identity (#129)
    * subnet container app env
    
    * fix gh runner identity
    
    * fix gh runner identity
    
    * fix gh runner identity

--- github_federated_identity/locals.tf
+++ github_federated_identity/locals.tf
@@ -1,7 +1,7 @@
 locals {
   name = var.domain == "" ? "${var.prefix}-${var.env_short}" : "${var.prefix}-${var.env_short}-${var.domain}"
 
-  resource_group_name = "${var.prefix}-${var.env_short}-identity-rg"
+  resource_group_name = var.identity_rg_name == null ? "${var.prefix}-${var.env_short}-identity-rg" : var.identity_rg_name
   identity_name       = var.app_name == "" ? "${local.name}-github-${var.identity_role}-identity" : "${local.name}-${var.app_name}-github-${var.identity_role}-identity"
   federation_prefix   = "${local.name}-github"
 }
