293151614bd6114e87658abf76c82e0febad9d82
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-09-05 16:50:53 +0200

    changed flow log target to subnet

--- network_security_group/main.tf
+++ network_security_group/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_network_watcher_flow_log" "network_watcher_flow_log" {
   resource_group_name  = var.flow_logs.network_watcher_rg
   name                 = "${var.prefix}-${each.key}-flow-log"
 
-  target_resource_id = azurerm_network_security_group.custom_nsg[each.key].id
+  target_resource_id = data.azurerm_subnet.subnet["${each.value.target_subnet_name}-${each.value.target_subnet_vnet_name}"].id
   storage_account_id = var.flow_logs.storage_account_id
   enabled            = each.value.watcher_enabled
 
