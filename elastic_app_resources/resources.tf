1e787339e73cb4549724ef4254cf3a42698add4d
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-11-07 10:24:48 +0100

    wip

--- elastic_app_resources/resources.tf
+++ elastic_app_resources/resources.tf
@@ -293,6 +293,11 @@ resource "elasticstack_kibana_alerting_rule" "alert" {
     }
 
     precondition {
+      condition     = can(each.value.custom_threshold) ? alltrue([for agg in each.value.custom_threshold.aggregations:  agg.name != null]) : true
+      error_message = "custom_threshold.aggregations.*.name must not be null. used by alert '${each.key}' in '${var.application_name}' application"
+    }
+
+    precondition {
       condition     = can(each.value.custom_threshold) ? lookup(each.value.custom_threshold, "equation", null) != null : true
       error_message = "custom_threshold must have equation defined. used by alert '${each.key}' in '${var.application_name}' application"
     }
