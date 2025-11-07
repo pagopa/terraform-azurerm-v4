2ce829f302edd837b409f34216ff2e9aa1c1e581
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-11-07 12:08:53 +0100

    wip

--- elastic_app_resources/resources.tf
+++ elastic_app_resources/resources.tf
@@ -318,7 +318,7 @@ resource "elasticstack_kibana_alerting_rule" "alert" {
     }
 
     precondition {
-      condition     = can(each.value.custom_threshold) && can(each.value.custom_threshold.group_by) ? length(lookup(each.value.custom_threshold, "group_by", [])) > 0 : true
+      condition     = can(each.value.custom_threshold) && lookup(each.value.custom_threshold, "group_by", []) != null ? length(lookup(each.value.custom_threshold, "group_by", [])) > 0 : true
       error_message = "custom_threshold.group_by must have at least one item if defined. used by alert '${each.key}' in '${var.application_name}' application"
     }
 
