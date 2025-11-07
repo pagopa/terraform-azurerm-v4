0e27e8f31e3012547e58b79e4d3009c7015d1209
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-11-07 12:01:28 +0100

    wip

--- elastic_app_resources/resources.tf
+++ elastic_app_resources/resources.tf
@@ -318,6 +318,11 @@ resource "elasticstack_kibana_alerting_rule" "alert" {
     }
 
     precondition {
+      condition     = can(each.value.custom_threshold) && can(each.value.custom_threshold.group_by) ? length(each.value.custom_threshold.group_by) > 0 : true
+      error_message = "custom_threshold.group_by must have at least one item if defined. used by alert '${each.key}' in '${var.application_name}' application"
+    }
+
+    precondition {
       condition     = can(each.value.custom_threshold) ? lookup(each.value.custom_threshold, "equation", null) != null : true
       error_message = "custom_threshold must have equation defined. used by alert '${each.key}' in '${var.application_name}' application"
     }
@@ -401,7 +406,7 @@ resource "elasticstack_kibana_alerting_rule" "alert" {
               lookup(agg, "field", null) != null ? {field = agg.field}: {})
           ]
         }]
-        groupBy: lookup(each.value.custom_threshold, "group_by", null),
+        groupBy: lookup(each.value.custom_threshold, "group_by", ""),
         alertOnNoData: length(try(each.value.custom_threshold.group_by, [])) > 0 ? false : lookup(each.value.custom_threshold, "alert_on_no_data", false)
         alertOnGroupDisappear: length(try(each.value.custom_threshold.group_by, [])) > 0 ? lookup(each.value.custom_threshold, "alert_on_no_data", false) : false
       } : null
