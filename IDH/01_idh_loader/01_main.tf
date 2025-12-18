2f4ffd15ebc9c138ef48801967568694c8684415
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-12-18 14:59:38 +0100

    added try

--- IDH/01_idh_loader/01_main.tf
+++ IDH/01_idh_loader/01_main.tf
@@ -1,7 +1,7 @@
 locals {
-  platform_env_tiers_configuration = yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/${var.env}/${var.idh_resource_type}.yml"))
-  platform_common_tiers_configuration = yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/common/${var.idh_resource_type}.yml"))
-  global_common_tiers_configuration = yamldecode(file("${path.module}/../00_product_configs/common/${var.idh_resource_type}.yml"))
+  platform_env_tiers_configuration = try(yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/${var.env}/${var.idh_resource_type}.yml")), {})
+  platform_common_tiers_configuration = try(yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/common/${var.idh_resource_type}.yml")), {})
+  global_common_tiers_configuration = try(yamldecode(file("${path.module}/../00_product_configs/common/${var.idh_resource_type}.yml")), {})
 
   tiers_configurations = merge(
     local.global_common_tiers_configuration,
