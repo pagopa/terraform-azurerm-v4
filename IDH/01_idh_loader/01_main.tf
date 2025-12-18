4d471833821ad2f5b893e78621eda8bb1e27ee59
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-12-18 14:54:14 +0100

    defined common handling

--- IDH/01_idh_loader/01_main.tf
+++ IDH/01_idh_loader/01_main.tf
@@ -1,5 +1,13 @@
 locals {
-  tiers_configurations = yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/${var.env}/${var.idh_resource_type}.yml"))
+  platform_env_tiers_configuration = yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/${var.env}/${var.idh_resource_type}.yml"))
+  platform_common_tiers_configuration = yamldecode(file("${path.module}/../00_product_configs/${var.product_name}/common/${var.idh_resource_type}.yml"))
+  global_common_tiers_configuration = yamldecode(file("${path.module}/../00_product_configs/common/${var.idh_resource_type}.yml"))
+
+  tiers_configurations = merge(
+    local.global_common_tiers_configuration,
+    local.platform_common_tiers_configuration,
+    local.platform_env_tiers_configuration
+  )
 
   envs = ["dev", "uat", "prod"]
 }
