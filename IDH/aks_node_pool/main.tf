012471381d6d702b8742a716559d968d17a15829
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2026-02-10 11:01:03 +0100

    feat: Add embedded subnet aks nodepool (#213)
    * add aks overlay snet
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * natgw association
    
    * fix static
    
    * fix static
    
    * fix static
    
    * fix static
    
    * fix static

--- IDH/aks_node_pool/main.tf
+++ IDH/aks_node_pool/main.tf
@@ -12,7 +12,7 @@ module "aks_node_pool" {
   # Core identifiers
   name                  = var.name
   kubernetes_cluster_id = var.kubernetes_cluster_id
-  vnet_subnet_id        = var.vnet_subnet_id
+  vnet_subnet_id        = var.embedded_subnet.enabled ? try(module.aks_overlay_snet[0].subnet_id, null) : var.vnet_subnet_id
 
   ###############################################################
   # Compute & Storage settings (safe‑lookup + try() wrapper)
@@ -50,4 +50,33 @@ module "aks_node_pool" {
   )
 
   tags = var.tags
+
+  depends_on = [module.aks_overlay_snet]
+}
+
+# IDH/subnet
+module "aks_overlay_snet" {
+  source = "../subnet"
+  count  = var.embedded_subnet.enabled ? 1 : 0
+
+  name                 = "${var.embedded_subnet.subnet_name}-aks-overlay-snet"
+  resource_group_name  = var.embedded_subnet.vnet_rg_name
+  virtual_network_name = var.embedded_subnet.vnet_name
+
+  env               = var.env
+  idh_resource_tier = "aks_overlay"
+  product_name      = var.product_name
+
+  nsg_flow_log_configuration   = var.nsg_flow_log_configuration
+  embedded_nsg_configuration   = var.embedded_nsg_configuration
+  create_self_inbound_nsg_rule = var.create_self_inbound_nsg_rule
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_nat_gateway_association" "aks_overlay_snet_nat_association" {
+  count = var.embedded_subnet.enabled ? 1 : 0
+
+  subnet_id      = module.aks_overlay_snet[0].subnet_id
+  nat_gateway_id = var.embedded_subnet.natgw_id
 }
--- kubernetes_cluster_node_pool/versions.tf
+++ kubernetes_cluster_node_pool/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3"
+      version = "~> 3.0"
     }
   }
 }
