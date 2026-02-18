0632fe9f2941145c69dfba0c58b39e2641e5d277
ffppa <fabio.felici@pagopa.it>
2026-02-18 10:27:59 +0100

    feat: [PAYMCLOUD-476] Introduce Log Analytics module with IDH (#221)
    * feat: Add Log Analytics Workspace module with environment-specific configurations for `cstar` and `pagopa` products
    
    - Introduced environment-specific configurations (`dev`, `uat`, `prod`) for `Log Analytics Workspace` resources, including retention policies, daily quotas, and private endpoints.
    - Added support for Application Insights integration with environment-specific settings.
    - Created Terraform modules, variables, and documentation to streamline configuration and deployment.
    
    * fix: Correct variable reference for
    
    * refactor: Simplify NSG rule configuration in Log Analytics Workspace by replacing custom NSG settings with `create_self_inbound_nsg_rule` flag
    
    * feat: Embed NSG flow log configuration into Log Analytics Workspace and add output for resource group
    
    - Removed obsolete variables: `traffic_analytics_law_name`, `traffic_analytics_law_rg`.
    - Enhanced `nsg_flow_log_configuration` with embedded settings in Log Analytics Workspace.
    - Included `resource_group` as an output for improved module usability.
    - Added `create_self_inbound_nsg_rule` to environment-specific configurations.
    
    * Add "Deprecated" column to Log Analytics Workspace documentation for `cstar` and `pagopa` configurations
    
    * feat: Add support for linked Log Analytics Workspace configurations
    
    - Introduced `linked_law_enabled` variable to enable conditional creation of linked Log Analytics Workspace.
    - Updated resource logic to handle both linked and standalone configurations using `local.created_law`.
    - Enhanced documentation and configuration files with `law_ignore_changes` field for linked clusters.
    - Adjusted outputs to reference `local.created_law` for consistent resource management.
    
    * feat: Enable internet ingestion and disable private endpoints for Log Analytics Workspace configurations
    
    - Updated `law_internet_ingestion_enabled` to `true` across all environments (`dev`, `uat`, `prod`) for `cstar` and `pagopa`.
    - Set `private_endpoint_enabled` to `false` uniformly across configurations.
    
    * Set `private_endpoint_enabled` to `false` across all Log Analytics Workspace configurations for `cstar` and `pagopa`.
    
    * Update `law_ignore_changes` references to `linked_law_enabled` across Log Analytics Workspace configurations and documentation
    
    * Fix typo in Log Analytics Workspace description: "Applicaiton Insight" to "Application Insight" and update linked cluster phrasing

---
+++ IDH/log_analytics_workspace/04_variables.tf
@@ -0,0 +1,135 @@
+variable "product_name" {
+  type        = string
+  description = "(Required) product_name used to identify the platform for which the resource will be created"
+  validation {
+    condition = (
+      length(var.product_name) <= 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
+
+variable "env" {
+  type        = string
+  description = "(Required) Environment for which the resource will be created"
+}
+
+variable "name" {
+  type        = string
+  description = "(Required) The name which should be used for this PostgreSQL Flexible Server. Changing this forces a new PostgreSQL Flexible Server to be created."
+}
+
+variable "location" {
+  type        = string
+  description = "(Required) The Azure Region where the PostgreSQL Flexible Server should exist."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "(Required) The name of the Resource Group where the PostgreSQL Flexible Server should exist."
+}
+
+variable "idh_resource_tier" {
+  type        = string
+  description = "(Required) The name od IDH resource key to be created."
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "(Required) A mapping of tags which should be assigned to the Resource."
+}
+
+variable "log_analytics_workspace_tables" {
+  type = map(object({
+    retention_in_days       = optional(number)
+    total_retention_in_days = optional(number)
+  }))
+  description = "A map of tables to create in the Log Analytics Workspace"
+  default     = {}
+}
+
+variable "create_application_insights" {
+  type        = bool
+  description = "Should the Application Insights be created?"
+  default     = true
+  validation {
+    condition = (
+      var.create_application_insights && var.application_insights_id != null ? false : true
+    )
+    error_message = "If 'create_application_insights' is true, 'application_insights_id' must be null. If 'application_insights_id' is provided, 'create_application_insights' must be false."
+  }
+}
+
+variable "application_insights_name" {
+  type        = string
+  description = "The name of the Application Insights. If creating, and not provided, it will be generated from the workspace name. If obtaining existing, this is required."
+  default     = null
+  validation {
+    condition = (
+      var.application_insights_id != null && var.application_insights_name == null ? false : true
+    )
+    error_message = "If 'application_insights_id' is provided (obtaining an existing resource), 'application_insights_name' is required."
+  }
+}
+
+variable "application_insights_id" {
+  type        = string
+  description = "The ID of an existing Application Insights resource. If provided, no new Application Insights will be created."
+  default     = null
+}
+
+variable "application_insights_resource_group_name" {
+  type        = string
+  description = "The Resource Group name of the existing Application Insights. If not provided, the workspace resource group will be used."
+  default     = null
+}
+
+variable "embedded_subnet" {
+  type = object({
+    enabled      = bool
+    vnet_name    = optional(string, null)
+    vnet_rg_name = optional(string, null)
+  })
+  description = "(Optional) Configuration for creating an embedded Subnet for the Log Analytics Workspace. If 'enabled' is true, 'subnet_id' must be null"
+  default = {
+    enabled      = false
+    vnet_name    = null
+    vnet_rg_name = null
+  }
+
+  validation {
+    condition     = var.embedded_subnet.enabled ? (var.embedded_subnet.vnet_name != null && var.embedded_subnet.vnet_rg_name != null) : true
+    error_message = "If 'embedded_subnet' is enabled, both 'vnet_name' and 'vnet_rg_name' must be provided."
+  }
+}
+
+variable "nsg_flow_log_configuration" {
+  type = object({
+    enabled              = bool
+    network_watcher_name = optional(string, null)
+    network_watcher_rg   = optional(string, null)
+    storage_account_id   = optional(string, null)
+  })
+  description = "(Optional) NSG flow log configuration"
+  default = {
+    enabled = false
+  }
+}
+
+variable "embedded_nsg_configuration" {
+  type = object({
+    source_address_prefixes      = list(string)
+    source_address_prefixes_name = string ## short name for source_address_prefixes
+  })
+  description = "(Optional) List of allowed cidr and name . Follows the format defined in https://github.com/pagopa/terraform-azurerm-v4/tree/main/network_security_group#rule-configuration"
+  default = {
+    source_address_prefixes : ["*"]
+    source_address_prefixes_name = "All"
+  }
+}
+
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "A list of Private DNS Zone IDs for the Private Endpoint."
+  default     = []
+}
