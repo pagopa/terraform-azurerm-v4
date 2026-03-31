58111b51fed3d33944ef0647b504a6db2c8339be
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2026-03-31 18:38:47 +0200

    adf principal id

--- data_factory/outputs.tf
+++ data_factory/outputs.tf
@@ -2,3 +2,7 @@ output "id" {
   description = "The id of the Data Factory."
   value       = azurerm_data_factory.this.id
 }
+
+output "principal_id" {
+  value = azurerm_data_factory.this.identity[0].principal_id
+}
