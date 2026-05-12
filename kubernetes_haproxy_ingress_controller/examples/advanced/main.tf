f2d22c80f6c0960cef58650c5deb6d808ded608f
ffppa <fabio.felici@pagopa.it>
2026-05-12 14:40:46 +0200

    feat: add canary ACL example and restrict Helm provider version to <3.0.0

--- kubernetes_haproxy_ingress_controller/examples/advanced/main.tf
+++ kubernetes_haproxy_ingress_controller/examples/advanced/main.tf
@@ -23,7 +23,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.11.0"
+      version = ">= 2.11.0, < 3.0.0"
     }
   }
 }
@@ -42,15 +42,6 @@ provider "kubernetes" {
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
   client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-  }
 }
 
 ###############################################################################
--- kubernetes_haproxy_ingress_controller/examples/basic/main.tf
+++ kubernetes_haproxy_ingress_controller/examples/basic/main.tf
@@ -17,7 +17,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.11.0"
+      version = ">= 2.11.0, < 3.0.0"
     }
   }
 }
@@ -30,22 +30,6 @@ provider "azurerm" {
 data "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_cluster_name
   resource_group_name = var.resource_group_name
-}
-
-provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-  }
 }
 
 ###############################################################################
