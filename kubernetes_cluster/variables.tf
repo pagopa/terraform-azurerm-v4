cbcc999f5c89e4a824d6286e35198be8fb856478
ffppa <fabio.felici@pagopa.it>
2026-07-31 10:20:19 +0200

    feat: Update AMA log collection settings to expand namespace exclusions and clear system pod log collection

--- kubernetes_cluster/variables.tf
+++ kubernetes_cluster/variables.tf
@@ -718,9 +718,9 @@ variable "ama_log_collection_settings" {
   description = "Container Insights (ama-logs) log collection settings. stdout and stderr can use different namespace exclusions, and coredns system pod logs are collected by default."
   type = object({
     enable_log_collection_cm    = optional(bool, false)
-    stdout_exclude_namespaces   = optional(list(string), ["gatekeeper-system"])
-    stderr_exclude_namespaces   = optional(list(string), ["gatekeeper-system"])
-    collect_system_pod_logs     = optional(list(string), ["kube-system:coredns"])
+    stdout_exclude_namespaces   = optional(list(string), ["kube-system", "gatekeeper-system"])
+    stderr_exclude_namespaces   = optional(list(string), ["kube-system", "gatekeeper-system"])
+    collect_system_pod_logs     = optional(list(string), [])
     enable_stdout_logs          = optional(bool, true)
     enable_stderr_logs          = optional(bool, true)
     containerlog_schema_version = optional(string, "v2")
